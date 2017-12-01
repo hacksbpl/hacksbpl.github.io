d1214e96ed8e7dab02df6e4b6bac9a686504f1a5
Shivan Modha <shivanmodha@gmail.com>
2017-12-01 03:06:56 -0500

    SBPL Sponsor & Subscribe

---
+++ events.js
@@ -0,0 +1,28 @@
+window.addEventListener("_event_onInitializeFirebase", (event) =>
+{
+    firebase.initializeApp(event.detail.configuration);
+});
+window.addEventListener("_event_onSetData", (event) =>
+{
+    firebase.database().ref(event.detail.reference).set(event.detail.data);
+});
+window.addEventListener("_event_onAppendData", (event) =>
+{
+    firebase.database().ref(event.detail.reference).push().set(event.detail.data);
+});
+window.addEventListener("_event_onUpdateData", (event) =>
+{
+    firebase.database().ref(event.detail.reference).update(event.detail.data);
+});
+window.addEventListener("_event_onGetData", (event) =>
+{
+    let sent = false;
+    firebase.database().ref(event.detail.reference).on("value", (e) =>
+    {
+        if (!sent)
+        {
+            sent = true;
+            event.detail.callback(e.val());
+        }
+    }); 
+});
